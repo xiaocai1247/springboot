903f1cbea2090870315f2703b1b23b71181fdab2
xiaocai1247 <2198321051@qq.com>
2023-05-03 22:36:29 +0800

    test

---
+++ src/main/java/com/xiaocai/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.xiaocai.controller;
+
+import com.xiaocai.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author xiaocai
+ * @date 2023/5/3 22:31
+ */
+public class UserController {
+
+    public List<User> list() {
+
+
+        return null;
+    }
+
+}
---
+++ src/main/java/com/xiaocai/pojo/User.java
@@ -0,0 +1,27 @@
+package com.xiaocai.pojo;
+
+/**
+ * @author xiaocai
+ * @date 2023/5/3 22:31
+ */
+public class User {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
